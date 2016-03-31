53dc2ff2317347ecee899092962323adf1dd58f1
dvonthenen <david.vonthenen@emc.com>
2016-03-31 14:45:17 -0700

    Update to allow all remote connections

--- docker/postgres/setup-remote.sh
+++ docker/postgres/setup-remote.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -i '/host    all             all             127.0.0.1\/32            trust/a host    all             all             172.17.0.1\/16            trust' /var/lib/postgresql/data/pg_hba.conf
+sed -i '/host    all             all             0.0.0.0\/0            md5/a host    all             all             172.17.0.1\/16            trust' /var/lib/postgresql/data/pg_hba.conf
